13733fe09d445b1a3a9ecdda45120b3e2b0d8b38
ебланище <nikel201009@gmail.com>
2025-09-14 22:10:02 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// Локальная версия - API отключен
+// Работаем только с локальной базой данных users_db.json
+
+const LOCAL_MODE = true;
+
+// Экспортируем для использования в main.js
+window.LOCAL_MODE = LOCAL_MODE;
