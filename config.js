3289b0ff3bfb05a043c71eb57584bc02eeacd541
ебланище <nikel201009@gmail.com>
2026-04-07 15:56:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,9 @@
-window.BOT_API_URL = "http://212.80.7.13:28742"; 
+// URL API бота (должен указывать на твой бот-сервер, где Flask API на порту 5000).
+// Можно переопределить через ?api=https://example.com в URL мини-аппа.
+window.BOT_API_URL = "http://212.80.7.13:28742";
+
+// LOCAL_MODE=false: не работаем как оффлайн-игрушка.
 window.LOCAL_MODE = false;
+
+// STRICT_REMOTE_BALANCE=true: баланс только через API бота (единый с Telegram-ботом).
+window.STRICT_REMOTE_BALANCE = true;
