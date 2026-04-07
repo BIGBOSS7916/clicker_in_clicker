205077284805200c9b872603003555f7a30fdef3
ебланище <nikel201009@gmail.com>
2026-04-07 18:07:38 +0300

    Update BOT_API_URL to new endpoint

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-window.BOT_API_URL = "http://de6.rubyhost.ru:28255";
+window.BOT_API_URL = "https://dodeper-api-proxy.nikel201009.workers.dev/";
 window.LOCAL_MODE = false;
 window.STRICT_REMOTE_BALANCE = true;
